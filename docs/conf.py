8621131f64b04713db199f5d35c49be9e4a77e59
arthnguy <98569363+arthnguy@users.noreply.github.com>
2025-03-28 20:01:08 -0700

    Update Sphinx config file

--- docs/conf.py
+++ docs/conf.py
@@ -40,5 +40,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+extensions = [
+    'sphinx_rtd_theme',
+]
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
